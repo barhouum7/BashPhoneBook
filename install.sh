24376c6923d344d31c10d1b5b4d8f66ab1b91ba5
barhouum7 <bb.med2000@gmail.com>
2020-12-23 23:29:54 +0100

    Added some changes:   install.sh

--- install.sh
+++ install.sh
@@ -19,21 +19,25 @@ checkroot
 printf "\n\e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Installing packages..\e[0m\n"
 
 
-if [[ -e mongodb-linux-*-4.4.2.tgz* ]]; then
-echo ""
-else
-
-if [[ ! -e mongodb-linux-*-4.4.2.tgz* ]]; then
-
 (trap '' SIGINT SIGTSTP && command -v mongo > /dev/null 2>&1 || { printf >&2  "\n\e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92mInstalling MongoDB, please wait...\n\e[0m"; apt-get update > /dev/null && wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.2.tgz && sudo apt-get install libcurl4 openssl liblzma5 curl wget -y && tar -zxvf ./mongodb-linux-*-4.4.2.tgz && sudo cp ./mongodb-linux-x86_64-ubuntu2004-4.4.2/bin/* /usr/local/bin/ > /dev/null || printf "\n\n\e[1;91mMongoDB NOT installed.\n\n\e[0m"; }) & wait $!
 
-else
-printf "\n\n\e[1;93m[!] Download Error... \e[0m\n\n"
-exit 1
-fi
-fi
+
+
+# if [[ -e mongodb-linux-*-4.4.2.tgz* ]]; then
+# echo ""
+# else
+
+# if [[ ! -e mongodb-linux-*-4.4.2.tgz* ]]; then
+
+
+
+# else
+# printf "\n\n\e[1;93m[!] Download Error... \e[0m\n\n"
+# exit 1
+# fi
+# fi
 
 
 
 printf "\n\e[32m Installed Successfully! ✔\e[0m\n\n"
-printf "\n\e[1;32m You are already have MongoDB Installed in your Machine! ✔\e[0m\n\n\n\e[1;94mInfo Of Installed Version:\e[0m\n\n" mongo --version
+printf "\n\e[1;32m You already have MongoDB Installed in your Machine! ✔\e[0m\n\n\n\e[1;94mInfo Of Installed Version:\e[0m\n\n" mongo --version
