dbde2b75deeb8fd8081120719e3c0ef1a9726f8d
barhouum7 <bb.med2000@gmail.com>
2020-12-28 03:07:05 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -17,12 +17,14 @@ fi
 checkroot
 
 printf "\n\e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Installing packages..\e[0m\n"
+sleep 1
 
+(trap '' SIGINT SIGTSTP && command -v mongo > /dev/null 2>&1 || { printf >&2  "\n\e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92mInstalling MongoDB, please wait...\n\e[0m"; apt-get update > /dev/null && wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.2.tgz && sudo apt-get install libcurl4 openssl liblzma5 curl wget -y && tar -zxvf ./mongodb-linux-x86_64-ubuntu2004-4.4.2.tgz && sudo cp ./mongodb-linux-x86_64-ubuntu2004-4.4.2/bin/* /usr/local/bin/ && sudo mkdir -p /var/lib/mongo && sudo mkdir -p /var/log/mongodb && mongod --dbpath /var/lib/mongo --logpath /var/log/mongodb/mongod.log --fork > /dev/null || printf "\n\n\e[1;91mMongoDB NOT installed.\n\n\e[0m"; }) & wait $!
 
-(trap '' SIGINT SIGTSTP && command -v mongo > /dev/null 2>&1 || { printf >&2  "\n\e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92mInstalling MongoDB, please wait...\n\e[0m"; apt-get update > /dev/null && wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-ubuntu2004-4.4.2.tgz && sudo apt-get install libcurl4 openssl liblzma5 curl wget -y && tar -zxvf ./mongodb-linux-x86_64-ubuntu2004-4.4.2.tgz && sudo cp ./mongodb-linux-x86_64-ubuntu2004-4.4.2/bin/* /usr/local/bin/ > /dev/null || printf "\n\n\e[1;91mMongoDB NOT installed.\n\n\e[0m"; }) & wait $!
-
+sleep 1
 printf "\n\e[32m - Installed Successfully! ✔\e[0m\n\n"
 printf "\n\e[1;32m - You already have MongoDB Installed in your Machine! ✔\e[0m\n\n\n\e[1;94mInfo Of Installed Version:\e[0m\n\n"
+sleep 1
 mongo --version
 
 
@@ -46,6 +48,8 @@ mongo --version
 
 
 
+sleep 1
 printf "\n\e[32m - Installed Successfully! ✔\e[0m\n\n"
 printf "\n\e[1;32m - You already have Mongo Shell Installed in your Machine! ✔\e[0m\n\n\n\e[1;94mInfo Of Installed Version:\t\e[0m"
+sleep 1
 mongosh --version
