c29794842347533e73d1cd915072d5fb84dc1d75
barhouum7 <bb.med2000@gmail.com>
2020-12-31 23:35:02 +0100

    modified:   dbScript.js

--- dbScript.js
+++ dbScript.js
@@ -1,11 +1,24 @@
 print("\n\t\t--->> Running testMyDb.js")
-db.stats()
-
 if (db.stats()) {
 	print("\n\t\t--->> MongoDb Running.")
 }else print("\n\t\t--->> MongoDb NOT Running.")
 
+db.stats()
+
 print("\n\t\t--->> Available DataBases.")
 show dbs;
-print("\n <<-------------------- Creating a New Collection -------------------->>")
-db.createCollection("contactRecord", { capped: true, size: 6142800, max: 10000 } );
+print("\n\t\t--->> Available Collections.")
+show collections;
+
+
+db.contacts.insertOne(
+{
+    firstName: $contact_firstName,
+    lastName: $contact_lastName,
+    gender: $contact_gender,
+    phoneNumber: $contact_number,
+    email: $contact_email,
+    address: $contact_address
+    
+  }
+)
